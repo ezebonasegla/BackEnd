e43fee5e366b77828e470017bdae9962a449ea59
Ezequiel Bonasegla <82910644+ezebonasegla@users.noreply.github.com>
2022-10-26 04:03:04 -0300

    Delete nginx-1.23.2 directory

--- nginx-1.23.2/desafio13/utils/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export function isValidPassword(user, password) {
-    return bcrypt.compareSync(password, user.password);
-}
-
-export function createHash(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-}
